036021179a534af44939288078ac3566ab2098e2
monawer <monawer@monawer.com>
2026-05-20 18:05:53 +0300

    Update TechAdvisorChat.tsx

--- src/components/landing/TechAdvisorChat.tsx
+++ src/components/landing/TechAdvisorChat.tsx
@@ -48,7 +48,7 @@ export function TechAdvisorChat() {
     setMessages(next);
     setLoading(true);
     try {
-      const url = joinUrl(serviceConfig.n8nUrl, "webhook-test/tech-advisor");
+      const url = joinUrl(serviceConfig.n8nUrl, "webhook/tech-advisor");
       const r = await fetch(url, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
